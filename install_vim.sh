c0933ec07f5e8f628323302979c5e3bbd85b2000
Srinivas Nyayapati <shireenrao@gmail.com>
2014-05-20 13:55:29 -0400

    Rearrange install scripts

--- install_vim.sh
+++ install_vim.sh
@@ -38,42 +38,6 @@ fi
 cd $DOTFILES_ROOT
 
 echo ""                                                                         
-echo "Begin install powerline fonts"                                            
-cd $DOTFILES_ROOT/tmp                                                           
-echo "Get SourceCodePro Fonts for powerline"                                    
-git clone https://github.com/Lokaltog/powerline-fonts                           
-if [ $? -eq 0 ];then                                                            
-    if [ -d $DOTFILES_ROOT/tmp/powerline-fonts/SourceCodePro ];then             
-        echo "Copying SourceCodePro Fonts"                                      
-        cp -f $DOTFILES_ROOT/tmp/powerline-fonts/SourceCodePro/*.otf $HOME/Library/Fonts
-        echo "Copy Fonts complete!"                                             
-    else                                                                        
-        echo "FAIL: copying fonts!!"                                            
-    fi                                                                          
-else                                                                            
-    echo "FAIL: git clone powerline-fonts"                                      
-fi    
-
-echo ""                                                                         
-#install Terminal Theme                                                         
-echo "Git clone Solarize theme"                                                 
-git clone https://github.com/altercation/solarized                              
-if [ $? -eq 0 ];then                                                            
-    if [ -d $DOTFILES_ROOT/tmp/solarized/osx-terminal.app-colors-solarized/xterm-256color ]; then
-        cd $DOTFILES_ROOT/tmp/solarized/osx-terminal.app-colors-solarized/xterm-256color
-        echo "Import Solarized Darm theme"                                      
-        open "Solarized Dark xterm-256color.terminal"                           
-        sleep 1 # Wait a bit to make sure the theme is loaded                   
-        echo "Change theme to Solarized Dark"                                   
-        osascript -e "tell application \"Terminal\" to set current settings of front window to settings set \"Solarized Dark 
-        #defaults write com.apple.terminal "Default Window Settings" -string "Solarized Dark xterm-256color"
-        #defaults write com.apple.terminal "Startup Window Settings" -string "Solarized Dark xterm-256color"
-        #cp $DOTFILES_ROOT/.com.apple.Terminal.plist ~/Library/Preferences/com.apple.Terminal.plist
-        echo "Theme settings complete!"                                         
-    fi                                                                          
-fi
-
-echo ""                                                                         
 cd $DOTFILES_ROOT                                                               
 #install vim plugins                                                            
 echo "init and update local vim plugin submodules from Github"                  
